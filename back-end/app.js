0dac5633602d4325c84464a8f943b43af6be9e58
Rawan Tabaja <rawantabaja12@gmail.com>
2022-06-28 11:36:30 +0200

    OK: Connection Mongo DB + user model

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,7 @@
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const cors = require("cors");
+const db = require("./models");
 
 const app = express();
 var corsOptions = {
@@ -9,6 +11,19 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Otakool API" });
---
+++ back-end/config/db.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: "mongodb+srv://"
+    + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD + "@cluster0.4lzjjdo.mongodb.net/?retryWrites=true&w=majority"
+};
